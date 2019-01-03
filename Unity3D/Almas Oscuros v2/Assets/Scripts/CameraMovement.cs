caeb576a0a8ee3d159defbe42bea68e4f99aa734
Fritz Dittmer <fdittmer9762@gmail.com>
2019-01-02 18:24:58 -0700

    Camera
    Added Camera and Camera Functionality

---
+++ Unity3D/Almas Oscuros v2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float smoothValue, lookSensitivity;
+    public Transform camTarget;
+    private Vector3 currentRot, tempRot;
+
+	void Update () {
+        this.transform.position = Vector3.Lerp(this.transform.position, camTarget.position, smoothValue);
+        RotateCamera();
+    }
+
+    void RotateCamera() {
+        currentRot = transform.rotation.eulerAngles;
+        currentRot.z = 0;
+        transform.rotation = Quaternion.Euler(currentRot);
+
+        tempRot.y = -Input.GetAxis("Mouse X");
+        tempRot.x = Input.GetAxis("Mouse Y");
+        tempRot.z = 0;
+        transform.Rotate(tempRot * Time.deltaTime * lookSensitivity);
+    }
+}
