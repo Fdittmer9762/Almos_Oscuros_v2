e2f5881c0c58d9a698a8a1edcb52e3d0973e935b
Fritz Dittmer <fdittmer9762@gmail.com>
2019-01-03 15:04:22 -0700

    Target Raycasting
    Can find targets and check for visibility of targets. 
    implementing camera movement and follow next

--- Unity3D/Almas Oscuros v2/Assets/Scripts/CameraMovement.cs
+++ Unity3D/Almas Oscuros v2/Assets/Scripts/CameraMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
     public float smoothValue, lookSensitivity;
-    public Transform camTarget;
+    public Transform camTarget, camera;
     private Vector3 currentRot, tempRot;
+
+    private Transform locTarget;
+    public LayerMask elm,glm;
+    public float maxRad = 8f;
 
 	void Update () {
         this.transform.position = Vector3.Lerp(this.transform.position, camTarget.position, smoothValue);
         RotateCamera();
+        if (Input.GetButtonDown("Fire1")) {
+            StartCoroutine(FindTarget());
+        }
     }
 
     void RotateCamera() {
@@ -31,5 +38,29 @@ public class CameraMovement : MonoBehaviour {
         tempRot.z = 0;
 
         transform.Rotate(tempRot * Time.deltaTime * lookSensitivity);
+    }
+
+    IEnumerator FindTarget()
+    {//Issues with raycasting
+        RaycastHit hit;
+        //if (Physics.Raycast(camera.position, camera.forward, out hit, 100.0f))
+        bool found = false;
+        float rad = .5f;
+        while (rad < maxRad) {
+            if (Physics.SphereCast(camera.position, rad, camera.forward, out hit, 100.0f, elm)) {
+                print("enemy found");
+                RaycastHit grndHit;
+                if (Physics.Linecast(camera.position, hit.transform.position, out grndHit, glm)) {
+                    print("but there's somenthing in the way!");
+                } else {
+                    print(hit.collider + " enemy is visible");
+                    break;
+                }
+            }
+            rad = rad + 1f;
+            yield return null;
+        }
+
+        yield return null;
     }
 }
