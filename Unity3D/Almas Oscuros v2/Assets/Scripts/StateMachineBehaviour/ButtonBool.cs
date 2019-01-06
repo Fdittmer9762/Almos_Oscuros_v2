b36bc230375de517ced4188022e7a8bdd93129e0
Fritz Dittmer <fdittmer9762@gmail.com>
2019-01-06 00:16:31 -0700

    AttackAnimationsVersion2
    Still having issues with the attack animations. 
    May resolve after character animations are in.
    Character does not attack towards enemy, needs to be resolved.

---
+++ Unity3D/Almas Oscuros v2/Assets/Scripts/StateMachineBehaviour/ButtonBool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBool : StateMachineBehaviour
+{
+
+    public string AnimParam, inputButton;
+
+    public bool startState, pressState;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(AnimParam, startState);
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetButtonDown(inputButton)) {
+            animator.SetBool(AnimParam, pressState);
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(AnimParam, startState);
+    }
+
+}
